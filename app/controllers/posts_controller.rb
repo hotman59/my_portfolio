f3299270cf6ddb394a670bd895b293920f67050b
othman <benrahmaneothman@gmail.com>
2016-03-07 12:29:19 +0100

    added and finich post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
   end
 
@@ -15,9 +16,32 @@ class PostsController < ApplicationController
       render 'new', notice: "bye error"
     end
   end
+def show
+end
+
+def edit
+end
+
+def update
+  if @post.update post_params
+    redirect_to @post, notice: "yoour article was succesfull saved"
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+@post.destroy
+redirect_to posts_path
+end
+
 
   private
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+      @post = Post.find(params[:id])
   end
 end
