5757351bfee68a4a974cf4c5e538a02e5c4d3b15
othman <benrahmaneothman@gmail.com>
2016-03-08 14:55:08 +0100

    create page project, create projects_controller create db(title desciption and url

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,5 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all.order("created_at desc")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307100926) do
+ActiveRecord::Schema.define(version: 20160308132324) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "link"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
