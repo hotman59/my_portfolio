8447e1617075682ed903493a112f5b0489017bb5
othman <benrahmaneothman@gmail.com>
2016-03-08 15:49:58 +0100

    create form in new.html.erb for project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,24 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all.order("created_at desc")
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+      @project = Project.new project_params
+
+      if @project.save
+        redirect_to @project, notice: "that project was succesfully saved!"
+      else
+        render 'new'
+      end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :link)
+  end
 end
