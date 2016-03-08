4eda5caa6fccdd9474924df41ff4aa5619d35ba5
othman <benrahmaneothman@gmail.com>
2016-03-08 13:18:17 +0100

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :contacts, only: [:new, :create]
+  resources :contacts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
